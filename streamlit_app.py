2a41ad5bb1cfaaa9821b6837139ce4935812d0ac
PrayashaM <prayasha04@gmail.com>
2025-08-31 11:31:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 # Import python packages
 import streamlit as st
+import requests
 cnx=st.connection("snowflake")
 session=cnx.session()
-import requests
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
